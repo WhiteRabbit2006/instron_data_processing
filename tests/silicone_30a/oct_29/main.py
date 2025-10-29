e1dc231901901180738cc2593e8dd9d9e8b0bf68
Mitchell Blaha <mitchell@blahafamily.com>
2025-10-29 11:24:37 -0500

    analyze 30a

---
+++ tests/silicone_30a/oct_29/main.py
@@ -0,0 +1,66 @@
+# silicone_30a/oct_29/main.py
+
+import os
+import sys
+
+# Find analysis_lib folder
+script_path = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(script_path, '..', '..', '..'))
+sys.path.append(project_root)
+
+from analysis_lib import workflow
+
+test_config = {
+    'data_file_name': 'Test1.steps.tracking.csv',
+
+    'test_recipe': [
+#        {'name': 'axial', 'end_time': 10.0, 'type': 'AXIAL'},
+    ],
+
+    'geometry': {
+        'gauge_length_mm': 110.0,
+        'axial_width_mm': 30.0,
+        'axial_thickness_mm': 2.65,
+        'torsional_side1_mm': 30.0,
+        'torsional_side2_mm': 2.65
+    },
+
+    'plots': [
+        {
+            'type': 'static',
+            'phases': ['__full__'],
+            'x_col': 'time',
+            'y_col': 'displacement',
+            'title': '{y_col} vs. {x_col}',
+            'output_filename': '{phase_name}_{y_col}_{x_col}'
+        },
+        {
+            'type': 'static',
+            'phases': ['__full__'],
+            'x_col': 'time',
+            'y_col': 'force',
+            'title': '{y_col} vs. {x_col}',
+            'output_filename': '{phase_name}_{y_col}_{x_col}'
+        },
+        {
+            'type': 'static',
+            'phases': ['__full__'],
+            'x_col': 'displacement',
+            'y_col': 'force',
+            'title': '{y_col} vs. {x_col}',
+            'output_filename': '{phase_name}_{y_col}_{x_col}'
+        },
+        {
+            'type': 'animated',
+            'phases': ['__full__'],
+            'x_col': 'displacement',
+            'y_col': 'force',
+            'title': '{y_col} vs. {x_col}',
+            'output_filename': '{phase_name}_{y_col}_{x_col}'
+        }
+        ]
+}
+
+# Run the program
+if __name__ == "__main__":
+    workflow.run_analysis_workflow(script_path, test_config)
